db1eb2d5020d885984d475d07987f6b8a32d0afd
Vítor Cavaleiro <up202004724@edu.fe.up.pt>
2023-11-27 19:47:41 +0000

    Update gradle dependencies

--- WishCloud/build.gradle.kts
+++ WishCloud/build.gradle.kts
@@ -12,6 +12,9 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.xerial:sqlite-jdbc:3.44.0.0")
+    implementation("org.slf4j:slf4j-simple:1.7.36")
+
 }
 
 tasks.test {
