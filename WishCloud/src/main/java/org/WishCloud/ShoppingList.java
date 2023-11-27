01ac3714f553507e7a3380995e25daa5226c5a5b
Ricardo Ribeiro <71141428+rnribeiro@users.noreply.github.com>
2023-11-27 22:17:33 +0000

    Initial setup of shoppinglist.java

---
+++ WishCloud/src/main/java/org/WishCloud/ShoppingList.java
@@ -0,0 +1,43 @@
+package org.WishCloud;
+
+import java.util.Map;
+
+public class ShoppingList {
+    // declare variables
+    private String listID;
+    private Map<String, CRDT<String>> listItems;
+
+    // constructor
+    public ShoppingList(String listID, Map<String, CRDT<String>> listItems) {
+        this.listID = listID;
+        this.listItems = listItems;
+    }
+
+    // getters and setters
+    public String getListID() {
+        return listID;
+    }
+
+    public void setListID(String listID) {
+        this.listID = listID;
+    }
+
+    public Map<String, CRDT<String>> getListItems() {
+        return listItems;
+    }
+
+    public void setListItems(Map<String, CRDT<String>> listItems) {
+        this.listItems = listItems;
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return "ShoppingList{" +
+                "listID='" + listID + '\'' +
+                ", listItems=" + listItems +
+                '}';
+    }
+
+}
+
