4d30ef8e6989e72fabe26705aad763a1e7224969
Vítor Cavaleiro <up202004724@edu.fe.up.pt>
2023-12-05 12:51:56 +0000

    Add gradle dependencies

--- WishCloud/build.gradle.kts
+++ WishCloud/build.gradle.kts
@@ -14,7 +14,9 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("org.xerial:sqlite-jdbc:3.44.0.0")
     implementation("org.slf4j:slf4j-simple:1.7.36")
-
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.0")
+    implementation("com.fasterxml.jackson.core:jackson-core:2.16.0")
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.16.0")
 }
 
 tasks.test {
