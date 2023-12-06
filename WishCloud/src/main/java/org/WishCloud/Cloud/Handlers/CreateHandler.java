6aee4742baf3928881d669773df7b73fa30c5e79
Vítor Cavaleiro <up202004724@edu.fe.up.pt>
2023-12-06 22:35:34 +0000

    Initial implementation of Handler for create resource

--- WishCloud/src/main/java/org/WishCloud/Cloud/Handlers/CreateHandler.java
+++ WishCloud/src/main/java/org/WishCloud/Cloud/Handlers/CreateHandler.java
@@ -1,26 +1,78 @@
 package org.WishCloud.Cloud.Handlers;
 
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
+import org.WishCloud.Database.SQl;
+import org.WishCloud.ShoppingList.ShoppingList;
+import org.WishCloud.Utils.Ring;
+import org.WishCloud.Utils.Serializer;
 
 import java.io.IOException;
-import java.io.OutputStream;
+import java.net.URI;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
+import java.util.Map;
 
-public class CreateHandler implements HttpHandler {
+import java.net.http.HttpClient;
+import com.sun.net.httpserver.HttpExchange;
+
+public class CreateHandler extends ServerHandler {
+    public CreateHandler(String serverName, Ring ring, SQl db) {
+        super(serverName, ring, db);
+    }
+
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        // Get the output stream to write the response
-        OutputStream os = exchange.getResponseBody();
+        // get attributes from the exchange
+        String method = exchange.getRequestMethod();
+        String path = exchange.getRequestURI().getPath();
+        Map<String,String> params = queryToMap(exchange.getRequestURI().getQuery());
+        byte[] body = exchange.getRequestBody().readAllBytes();
 
-        // Set the response headers
-        exchange.getResponseHeaders().set("Content-Type", "text/plain");
-        exchange.sendResponseHeaders(200, 0);
+        if (!method.equals("POST") || !path.equals("/create")
+                || !params.containsKey("uuid") || !params.containsKey("author")) {
+            sendResponse(exchange, 400, "Invalid request!");
+            return;
+        }
 
-        // Write the response
-        String response = "Hello, this is a simple HTTP server!";
-        os.write(response.getBytes());
 
-        // Close the output stream
-        os.close();
+        // check if the shopping list is null or already exists in database
+        ShoppingList shoppingList;
+        if (getDb().getShoppingList(params.get("uuid")) == null) {
+            sendResponse(exchange, 400, "Shopping list already exists!");
+            return;
+        } else if ((shoppingList = Serializer.deserialize(body)) == null) {
+            sendResponse(exchange, 400, "Shopping list corrupted!");
+            return;
+        }
+
+        int replicas = 2;
+        List<String> preferenceList = getRing().getPreferenceList(params.get("uuid"), replicas);
+        if (preferenceList.get(0).equals(getServerName())) {
+            for (String server : preferenceList.subList(1, preferenceList.size())) {
+                // send the shopping list to the next server in the ring
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create("http://" + server + "/create?uuid=" + params.get("uuid") + "&author=" + getServerName()))
+                        .POST(HttpRequest.BodyPublishers.ofByteArray(body))
+                        .build();
+
+                try {
+                    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                    if (response.statusCode() == 200) {
+                        System.out.println("\nReplica in " + server + " created! Server Response: " + response.body());
+                        replicas--;
+                    }
+                } catch (InterruptedException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+
+        // save the shopping list to the database
+        if (getDb().insertSL(shoppingList)) {
+            sendResponse(exchange, 500, "Error loading shopping list on database!");
+        }
+
+        sendResponse(exchange, 200, "Shopping list created!");
     }
 }
---
+++ WishCloud/src/main/java/org/WishCloud/Cloud/Handlers/ServerHandler.java
@@ -0,0 +1,64 @@
+package org.WishCloud.Cloud.Handlers;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import org.WishCloud.Database.SQl;
+import org.WishCloud.Utils.Ring;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class ServerHandler implements HttpHandler {
+    protected final String serverName;
+    protected final Ring ring;
+    protected final SQl db;
+
+    public ServerHandler(String serverName, Ring ring, SQl db) {
+        this.serverName = serverName;
+        this.ring = ring;
+        this.db = db;
+    }
+
+    protected static Map<String, String> queryToMap(String query){
+
+        Map<String, String> result = new HashMap<String, String>();
+
+        for (String param : query.split("&")) {
+            String[] pair = param.split("=");
+            if (pair.length>1) { result.put(pair[0], pair[1]); }
+            else{ result.put(pair[0], ""); }
+        }
+
+        return result;
+
+    }
+
+    protected static void sendResponse(HttpExchange exchange, int statusCode, String response) {
+        try {
+            exchange.getResponseHeaders().set("Content-Type", "text/plain");
+            exchange.sendResponseHeaders(statusCode, 0);
+
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public SQl getDb() {
+        return db;
+    }
+
+    public Ring getRing() {
+        return ring;
+    }
+
+    public String getServerName() {
+        return serverName;
+    }
+}
