b0836ac4788817b69b61aee09511dd19193de1e9
Ricardo Ribeiro <71141428+rnribeiro@users.noreply.github.com>
2023-12-06 23:45:39 +0000

    updating cloud setup

--- WishCloud/src/main/java/org/WishCloud/Cloud/Cloud.java
+++ WishCloud/src/main/java/org/WishCloud/Cloud/Cloud.java
@@ -6,12 +6,16 @@ public class Cloud {
 
     public static void main(String[] args) {
         // Define seeds for the ring
-        List<String> seeds = List.of("localhost:8000", "localhost:8001");
+        List<String> seeds = List.of("localhost:8000", "localhost:8001", "localhost:8002");
 
-        // Instantiate and start three servers
-        startServer("localhost", 8000, seeds);
-        startServer("localhost", 8001, seeds);
-        startServer("localhost", 8002, seeds);
+        // Loop through the seeds and start a server for each one
+        for (String seed : seeds) {
+            String[] serverInfo = seed.split(":");
+            String serverIp = serverInfo[0];
+            int serverPort = Integer.parseInt(serverInfo[1]);
+            startServer(serverIp, serverPort, seeds);
+        }
+
     }
 
     private static void startServer(String serverIp, int serverPort, List<String> seeds) {
