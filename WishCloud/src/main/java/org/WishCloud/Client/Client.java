2228121da2d55f5434af7a6fd41d919cad03bae0
Ricardo Ribeiro <71141428+rnribeiro@users.noreply.github.com>
2023-12-08 13:41:26 +0000

    added list options menu and updated getlistfromserver http library

--- WishCloud/src/main/java/org/WishCloud/Client/Client.java
+++ WishCloud/src/main/java/org/WishCloud/Client/Client.java
@@ -3,10 +3,7 @@ package org.WishCloud.Client;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URI;
-import java.net.URL;
-import java.net.URLEncoder;
+import java.net.*;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -14,6 +11,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 import java.util.UUID;
+
+import org.WishCloud.CRDT.CRDT;
 import org.WishCloud.Client.UI.ShoppingInterface;
 import org.WishCloud.Database.SQl;
 import org.WishCloud.ShoppingList.ShoppingList;
@@ -41,24 +40,11 @@ public class Client {
 
         db = new SQl(clientUUID); // Create a database instance specific to this client
         db.createDB(); // Create the database
-        db.connect();
+        db.connect(); // Connect to the database
         Scanner scanner = new Scanner(System.in);
 
-        while (true) {
-            ShoppingInterface.printMainMenu();
-            int choice = scanner.nextInt();
+        mainMenu(scanner);
 
-            switch (choice) {
-                case 1:
-                    handleCreateList(scanner);
-                    break;
-                case 2:
-                    handleAccessList(scanner);
-                    break;
-                default:
-                    ShoppingInterface.displayInvalidChoice();
-            }
-        }
     }
 
     public static String generateUUID() {
@@ -66,13 +52,41 @@ public class Client {
         return uuid.toString();
     }
 
+    private static void mainMenu(Scanner scanner) {
+        while (true) {
+            ShoppingInterface.printMainMenu();
+
+            // try to read user input as an integer
+            int choice;
+            try {
+                choice = scanner.nextInt();
+            } catch (Exception e) {
+                ShoppingInterface.displayInvalidChoice();
+                scanner.next();
+                continue;
+            }
+
+            switch (choice) {
+                case 1:
+                    handleCreateList(scanner);
+                    break;
+                case 2:
+                    handleAccessList(scanner, null);
+                    break;
+                default:
+                    ShoppingInterface.displayInvalidChoice();
+            }
+        }
+    }
+
     private static void handleCreateList(Scanner scanner) {
         String listName = ShoppingInterface.promptForListName(scanner);
         String listUUID = UUID.randomUUID().toString();
 
         ShoppingList shoppingList = new ShoppingList(listName, listUUID, new HashMap<>());
+        db.connect();
         db.insertSL(shoppingList);
-
+//        db.close();
         byte[] serializedList = Serializer.serialize(shoppingList);
         boolean syncSuccess = synchronizeListWithServer(listUUID, serializedList);
 
@@ -83,6 +97,7 @@ public class Client {
     private static boolean synchronizeListWithServer(String listUUID, byte[] serializedList) {
         try {
             List<String> preferenceList = ring.getPreferenceList(listUUID, 3);
+            int missingReplicas = 3 - preferenceList.size();
             for (String server : preferenceList) {
 
                 HttpClient client = HttpClient.newHttpClient();
@@ -97,17 +112,17 @@ public class Client {
                         System.out.println("\nReplica in " + server + " created! Server Response: " + response.body());
                         return true;
                     }
-                } catch (InterruptedException e) {
-                    System.out.println(e.getMessage());
+                } catch (ConnectException | InterruptedException e) {
+                    missingReplicas--;
                 }
 
             }
+            return missingReplicas == 0;
 
         } catch (IOException e) {
             e.printStackTrace();
             return false;
         }
-        return false;
     }
 
     private static ShoppingList getListFromServerOrLocal(String listUUID) {
@@ -115,46 +130,198 @@ public class Client {
         if (shoppingList == null) {
             db.connect();
             shoppingList = db.getShoppingList(listUUID);
-            db.close();
+//            db.close();
         }
         return shoppingList;
     }
 
-    public static void handleAccessList(Scanner scanner) {
-        String listUUID = ShoppingInterface.promptForListUUID(scanner);
+    public static void handleAccessList(Scanner scanner, String listUUID) {
+        if (listUUID == null) {
+            listUUID = ShoppingInterface.promptForListUUID(scanner);
+        }
         ShoppingList shoppingList = getListFromServerOrLocal(listUUID);
 
-        if (shoppingList != null && !shoppingList.getListItems().isEmpty()) {
+        if (shoppingList != null) {
             ShoppingInterface.displayShoppingList(shoppingList);
+            // Prompt user for list actions
+            while (true) {
+                System.out.println("List Actions:");
+                System.out.println("1- Add Item");
+                System.out.println("2- Remove Item");
+                System.out.println("3- Update Item");
+                System.out.println("4- Exit");
+                System.out.print("Enter your choice: ");
+                int choice;
+                try {
+                    choice = scanner.nextInt();
+                } catch (Exception e) {
+                    ShoppingInterface.displayInvalidChoice();
+                    scanner.next();
+                    continue;
+                }
+
+                switch (choice) {
+                    case 1:
+                        handleAddItem(scanner, shoppingList);
+                        break;
+                    case 2:
+                        handleRemoveItem(scanner, shoppingList);
+                        break;
+                    case 3:
+                        handleUpdateItem(scanner, shoppingList);
+                        break;
+                    case 4:
+                        mainMenu(scanner);
+                    default:
+                        ShoppingInterface.displayInvalidChoice();
+                }
+            }
         } else {
             ShoppingInterface.displayListNotFound();
         }
     }
 
+    private static void handleUpdateItem(Scanner scanner, ShoppingList shoppingList) {
+        System.out.print("Enter Item Name: ");
+        String itemName = scanner.next();
+        System.out.print("Enter New Item Value: ");
+        int itemValue = scanner.nextInt();
+
+        // create CRDT object
+        CRDT<String> crdtItem = new CRDT<>(Integer.toString(itemValue), System.currentTimeMillis(), clientUUID);
+        shoppingList.updateItem(itemName, crdtItem);
+
+        // try updating the list in the database
+        db.connect();
+        if (db.updateShoppingList(shoppingList)) {
+            System.out.println("Item locally updated successfully.");
+        } else {
+            System.out.println("Failed to update item.");
+        }
+//        db.close();
+        byte[] serializedList = Serializer.serialize(shoppingList);
+        boolean syncSuccess = updateListInCloud(shoppingList.getListID(), serializedList);
+        ShoppingInterface.displayItemUpdateSuccess(syncSuccess);
+        handleAccessList(scanner, shoppingList.getListID());
+    }
+
+    private static void handleRemoveItem(Scanner scanner, ShoppingList shoppingList) {
+        System.out.print("Enter Item Name: ");
+        String itemName = scanner.next();
+        shoppingList.removeItem(itemName);
+
+        // try updating the list in the database
+        db.connect();
+        if (db.updateShoppingList(shoppingList)) {
+            System.out.println("Item locally removed successfully.");
+        } else {
+            System.out.println("Failed to remove item.");
+        }
+//        db.close();
+
+        byte[] serializedList = Serializer.serialize(shoppingList);
+        boolean syncSuccess = updateListInCloud(shoppingList.getListID(), serializedList);
+        ShoppingInterface.displayItemRemovalSuccess(syncSuccess);
+        handleAccessList(scanner, shoppingList.getListID());
+    }
+
+    private static boolean updateListInCloud(String listID, byte[] serializedList) {
+        try {
+            List<String> preferenceList = ring.getPreferenceList(listID, 3);
+            int missingReplicas = 3 - preferenceList.size();
+            for (String server : preferenceList) {
+
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create("http://" + server + "/update?uuid=" + listID))
+                        .POST(HttpRequest.BodyPublishers.ofByteArray(serializedList))
+                        .build();
+
+                try {
+                    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                    if (response.statusCode() == 200) {
+                        System.out.println("\nReplica in " + server + " updated! Server Response: " + response.body());
+                        return true;
+                    }
+                } catch (ConnectException | InterruptedException e) {
+                    missingReplicas--;
+                }
+
+            }
+            return missingReplicas == 0;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private static void handleAddItem(Scanner scanner, ShoppingList shoppingList) {
+        System.out.print("Enter Item Name: ");
+        String itemName = scanner.next();
+        System.out.print("Enter Item Value: ");
+        int itemValue = scanner.nextInt();
+
+        // create CRDT object
+        CRDT<String> crdtItem = new CRDT<>(Integer.toString(itemValue), System.currentTimeMillis(), clientUUID);
+        shoppingList.addItem(itemName, crdtItem);
+
+        // try updating the list in the database
+        db.connect();
+        if (db.updateShoppingList(shoppingList)) {
+            System.out.println("Item locally added successfully.");
+        } else {
+            System.out.println("Failed to add item.");
+        }
+//        db.close();
+
+
+        byte[] serializedList = Serializer.serialize(shoppingList);
+        boolean syncSuccess = synchronizeListWithServer(shoppingList.getListID(), serializedList);
+        ShoppingInterface.displaySynchronizationStatus(syncSuccess);
+        handleAccessList(scanner, shoppingList.getListID());
+    }
+
     private static ShoppingList getListFromServer(String listUUID) {
         try {
-            URL url = new URL("http://" + SERVER_IP + ":" + SERVER_PORT + "/read?uuid=" + URLEncoder.encode(listUUID, "UTF-8"));
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
+            List<String> preferenceList = ring.getPreferenceList(listUUID, 3);
+            int serversDown = 0;
+            for (String server : preferenceList) {
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create("http://" + server + "/read?uuid=" + listUUID))
+                        .GET()
+                        .build();
 
-            int responseCode = conn.getResponseCode();
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-                try (InputStream inputStream = conn.getInputStream()) {
-                    int nRead;
-                    byte[] data = new byte[1024];
-                    while ((nRead = inputStream.read(data, 0, data.length)) != -1) {
-                        buffer.write(data, 0, nRead);
+                try {
+                    HttpResponse<InputStream> response = client.send(request, HttpResponse.BodyHandlers.ofInputStream());
+                    if (response.statusCode() == 200) {
+                        // Read the InputStream into a byte array
+                        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+                        int nRead;
+                        byte[] data = new byte[1024];
+                        while ((nRead = response.body().read(data, 0, data.length)) != -1) {
+                            buffer.write(data, 0, nRead);
+                        }
+
+                        // Deserialize the byte array into a ShoppingList
+                        byte[] serializedList = buffer.toByteArray();
+                        return Serializer.deserialize(serializedList);
                     }
+                } catch (InterruptedException | ConnectException e) {
+                    serversDown++;
                 }
-                byte[] serializedList = buffer.toByteArray();
-                return Serializer.deserialize(serializedList);
+            }
+            if (serversDown == 3) {
+                System.out.println("All servers are down. Please try again later.");
+                return null;
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
         return null;
     }
+
 
 
 
