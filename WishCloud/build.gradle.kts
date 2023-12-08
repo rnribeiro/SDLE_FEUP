18457d4861381e43bad9d3ccd4b7a9e766699f6b
Ricardo Ribeiro <71141428+rnribeiro@users.noreply.github.com>
2023-12-08 19:07:21 +0000

    change gradle to run in terminal specific classes

--- WishCloud/build.gradle.kts
+++ WishCloud/build.gradle.kts
@@ -1,22 +1,37 @@
 plugins {
     id("java")
+    application
 }
 
-group = "org.WishCloud"
-version = "1.0-SNAPSHOT"
+application {
+    applicationDefaultJvmArgs = listOf("-Dfile.encoding=UTF-8")
+
+    // Configuration for Client
+    tasks.create<JavaExec>("client") {
+        mainClass.set("org.WishCloud.Client.Client")
+        classpath = sourceSets["main"].runtimeClasspath
+        standardInput = System.`in`
+    }
+
+    // Configuration for Cloud
+    tasks.create<JavaExec>("cloud") {
+        mainClass.set("org.WishCloud.Cloud.Cloud")
+        classpath = sourceSets["main"].runtimeClasspath
+    }
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("org.xerial:sqlite-jdbc:3.44.0.0")
     implementation("org.slf4j:slf4j-simple:1.7.36")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.16.0")
     implementation("com.fasterxml.jackson.core:jackson-core:2.16.0")
     implementation("com.fasterxml.jackson.core:jackson-annotations:2.16.0")
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
 }
 
 tasks.test {
